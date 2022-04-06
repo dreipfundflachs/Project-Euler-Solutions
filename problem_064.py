874339ea251f65bb2213c92d2000876ae48a0929
dreipfundflachs <zuehlkep@gmail.com>
2022-04-06 16:50:41 -0300

    included solutions to 064, 066

---
+++ problem_064.py
@@ -0,0 +1,46 @@
+#################################
+#  PROJECT EULER - PROBLEM 064  #
+#################################
+
+# The efficient algorithm for finding the partial denominators below is taken
+# from the section "Canonical form and repetend" of the Wikipedia page:
+# https://en.wikipedia.org/wiki/Periodic_continued_fraction
+import time
+from math import isqrt
+
+start = time.time()
+
+N = 10**4
+# We will consider the square root of n only when the latter is not a square.
+SQUARES = [n**2 for n in range(10**2 + 1)]
+NON_SQUARES = [n for n in range(2, N + 1) if n not in SQUARES]
+
+count = 0
+for n in NON_SQUARES:
+    a_0 = isqrt(n)
+
+    a = isqrt(n)
+    d = 1
+    m = 0
+
+    k = 1
+    period = 0
+    while a != 2 * a_0:
+        # The algorithm proceeds by repeated calculation of a (the current
+        # partial denominator) d and m, given their previous values according
+        # to these formulas and the initial values above (see the Wikipedia
+        # article).
+        m = d * a - m
+        d = (n - m**2) / d
+        a = int((a_0 + m) / d)
+
+        period += 1
+        k += 1
+
+    if period % 2 == 1:
+        count += 1
+
+print(count)
+
+end = time.time()
+print(f"Program runtime: {end - start} seconds")
--- problem_066.py
+++ problem_066.py
@@ -1,31 +1,56 @@
-# PROJECT EULER - PROBLEM 066
+#################################
+#  PROJECT EULER - PROBLEM 066  #
+#################################
 import time
 from math import isqrt
-from project_euler import is_square
+# The efficient algorithm for finding the partial denominators below is taken
+# from the section "Canonical form and repetend" of the Wikipedia page:
+# https://en.wikipedia.org/wiki/Periodic_continued_fraction
 
 
 start = time.time()
-n = 90
-maxd = 5
-maxx = 9
-for d in range(9, n):
-    if is_square(d):
-        continue
-    else:
-        x = isqrt(d + 1)
-        active = True
-        while active:
-            if isqrt((x**2 - 1) // d)**2 * d == x**2 - 1:
-                active = False
-                if x > maxx:
-                    maxd = d
-                    maxx = x
-            else:
-                x += 1
-                while x**2 % d != 1:
-                    x += 1
 
-print(maxx, maxd)
+N = 10**3
+SQUARES = [n**2 for n in range(10**2 + 1)]
+NON_SQUARES = [n for n in range(2, N + 1) if n not in SQUARES]
 
+max_n = 0
+max_solution = 0
+for n in NON_SQUARES:
+    p = []  # p[k] will be the numerator of the k-th convergent
+    q = []  # q[k] will be the denominatro of the k-th convergent
+    # We compute p[0], p[1], q[0] and q[1] outside of the loop to avoid
+    # negative indices in our list.
+    p.append(isqrt(n))
+    q.append(1)
+
+    a_0 = isqrt(n)
+    a = isqrt(n)
+    m = isqrt(n)
+    d = (n - m**2)
+    a = int(2 * a / d)
+    p.append(a * a_0 + 1)
+    q.append(a)
+
+    # For k >= 2, we calculate p[k] and q[k] according to the formulas:
+    # p[k] = a_k * p[k - 1] + p[k - 2] and q[k] = a_j * q[k - 1] + q[k - 2]
+    k = 2
+    period = 1
+    while p[k - 1]**2 - n * q[k - 1]**2 != 1:
+        # The algorithm to find a = a_k proceeds by repeated updating the
+        # values a, d and m, given their previous values according to the
+        # three formulas below (see the Wikipedia article).
+        m = d * a - m
+        d = (n - m**2) / d
+        a = int((a_0 + m) / d)
+        p.append(a * p[k - 1] + p[k - 2])
+        q.append(a * q[k - 1] + q[k - 2])
+        period += 1
+        k += 1
+    if p[k - 1] > max_solution:
+        max_solution = p[k - 1]
+        max_n = n
+
+print(max_n, max_solution)
 end = time.time()
 print(f"Program runtime: {end - start} seconds")
