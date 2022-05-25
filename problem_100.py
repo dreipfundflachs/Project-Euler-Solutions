0498c1d75c42e5d02ab0da7c4c42204977a53999
dreipfundflachs <zuehlkep@gmail.com>
2022-05-25 19:24:58 -0300

    refactor: comments to problem 100

--- problem_100.py
+++ problem_100.py
@@ -14,20 +14,20 @@ def gcd(a: int, b:  int) -> int:
 
 # Let b (resp. n) be the number of blue (resp. total) balls. Then we need to
 # find solutions to the equation b(b - 1) / (n(n - 1)) = 1 / 2. Given n, this
-# is a quadratic equation in b. The solution b will be a positive integer if
-# and only if n is such that (1 + 2 * n**2 -2 * n) = (n - 1)**2 + n**2
-# is a perfect square. Thus, we have reduced the problem to finding natural
-# numbers n such that (n - 1, n) will yield a primitive Pythagorean triple. We
-# know that any such triple is given by Euclid's formula:
+# is a quadratic equation in b. There will be integer solutions to this if and
+# only if n is such that (1 + 2 * n**2 -2 * n) = (n - 1)**2 + n**2
+# is a perfect square. This reduces the problem to finding natural numbers n
+# such that (n - 1, n) will yield a primitive Pythagorean triple. We know that
+# any such triple is given by Euclid's formula:
 # a = p**2 - q**2, b = 2 * p * q and c = p**2 + q**2, where p > q have gcd = 1
 # and exactly one of them is even. Thus we look for integers p, q satisfying
 # all of those conditions and for which in addition either a = b - 1 or
 # a = b + 1. Again, solving this for given q yields a quadratic equation in p,
-# which can thus easily be solved. For p to be an integer, we must have that
-# either 2 * q**2 + 1 or 2 * q**2 - 1 is a perfect square, respectively. Once
-# we find the first pair (p, q) satisfying these conditions for which
-# n = max(2 * p * q, p**2 - q**2) is larger than the given bound, we are done
-# and b is given by the solution of the original quadratic equation.
+# which can easily be solved. For p to be an integer, we must have that either
+# 2 * q**2 + 1 or 2 * q**2 - 1 is a perfect square, respectively. Once we find
+# the first pair (p, q) satisfying these conditions for which n = max(2 * p *
+# q, p**2 - q**2) is larger than the given bound, we are done and b is given by
+# the solution of the original quadratic equation.
 
 start = time.time()
 
