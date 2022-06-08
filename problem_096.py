5a097919f894176a06e197dfeb69fb52e6192cef
dreipfundflachs <zuehlkep@gmail.com>
2022-06-08 16:10:22 -0300

    refactor: problem 096

--- problem_096.py
+++ problem_096.py
@@ -6,21 +6,23 @@ from copy import deepcopy
 
 
 def get_available(grid: list[list[int]], cell: tuple[int]) -> set[int]:
-    """ Given a grid and a cell in it, encoded as a pair of integers, returns a
-    set comprising all available numbers to be placed at that position. """
+    """ Given a grid and one of its cells (encoded as a pair of integers),
+    returns a set comprising all available numbers to be placed at that
+    position. Returns the empty set if the cell is already occupied or if no
+    number is available. """
     (i, j) = cell
     if grid[i][j] != 0:
         # Cell is already occupied.
         return set()
     else:
         columns = transpose(grid)
-        # Extract all values inside the cells in the same box.
+        # Extract all values inside the cells in the same box:
         box_entries = {grid[m][n] for m in box(i) for n in box(j)}
-        # Get all available numbers in the same row, column and box.
+        # Get all available numbers in the same row, column and box:
         available_row = {d for d in NUMBERS if d not in grid[i]}
         available_column = {d for d in NUMBERS if d not in columns[j]}
         available_box = {d for d in NUMBERS if d not in box_entries}
-        # Take the intersection of the three preceding sets to find the answer.
+        # Take the intersection of the three preceding sets to find the answer:
         available = ((available_row
                      .intersection(available_column))
                      .intersection(available_box))
@@ -32,7 +34,7 @@ def find_minimizing(grid: list[list[int]]) -> tuple[int]:
         * If it is not completely filled (solved), find a cell at which the
         number of available entries is as small as possible.
         * If the grid is already filled, return (10, 10).
-        * If it cannot be solved, return None. """
+        * If it cannot be solved, return (-1, -1). """
     minimizing_cell = (10, 10)
     minimum = 10
     for i in INDICES:
@@ -45,23 +47,24 @@ def find_minimizing(grid: list[list[int]]) -> tuple[int]:
                 number_of_available = len(available)
                 if number_of_available == 0:
                     # Failure! Not possible to fill grid.
-                    return None
+                    return (-1, -1)
                 elif number_of_available < minimum:
-                    # Found a new minimum:
+                    # Found a new minimum.
                     minimizing_cell = (i, j)
                     minimum = number_of_available
     return minimizing_cell
 
 
 def solve(grid: list[list[int]]) -> list[list[int]]:
-    """ Given a Su Doku grid, returns the solution (as another grid). """
+    """ Given a Su Doku grid, returns the solution (as another grid) or an
+    empty list in case the grid cannot be solved. """
     cell = find_minimizing(grid)
     if cell == (10, 10):
         # Grid is already solved.
         return grid
-    elif cell is None:
+    elif cell == (-1, -1):
         # Grid cannot be solved.
-        return None
+        return []
     else:
         (i, j) = cell
         possibilities = get_available(grid, (i, j))
@@ -69,15 +72,16 @@ def solve(grid: list[list[int]]) -> list[list[int]]:
             new_grid = deepcopy(grid)
             new_grid[i][j] = k
             new_grid = solve(new_grid)
-            if new_grid is None:
-                # Impossible to solve the new grid.
+            if new_grid == []:
+                # Substitution of k in the (i, j) cell results in a new grid
+                # which cannot be solved. Try the next value of k.
                 continue
             elif new_grid != grid:
                 # Solved!
                 return new_grid
         # If this point has been reached, all attempted substitutions for the
-        # given cell failed: It is impossible to solve original grid!
-        return None
+        # given cell failed: It is thus impossible to solve original grid!
+        return []
 
 
 def box(i: int) -> set[int]:
@@ -85,9 +89,9 @@ def box(i: int) -> set[int]:
      which belong to the same box as i in a Su Doku grid (including i)."""
     if i in INDICES_1:
         return INDICES_1
-    elif i in INDICES_2:
+    if i in INDICES_2:
         return INDICES_2
-    else:
+    if i in INDICES_3:
         return INDICES_3
 
 
@@ -114,9 +118,8 @@ def solve_several(grids: list[list[list[int]]]) -> list[list[list[int]]]:
 def get_sum(grids: list[list[list[int]]]) -> int:
     """ Returns the sum of the upper-left three-digit numbers in all of the
     solved grids. """
-    solved_grids = solve_several(grids)
     total = 0
-    for grid in solved_grids:
+    for grid in solve_several(grids):
         current = ''
         for j in range(3):
             current += str(grid[0][j])
@@ -125,7 +128,7 @@ def get_sum(grids: list[list[list[int]]]) -> int:
 
 
 def display_grid(grid: list[list[int]]) -> None:
-    """ Displays a Su Doku grid, returns None. This function is not required
+    """ Displays a Su Doku grid and returns None. This function is not required
     for the solution of the problem. """
     for i in INDICES:
         print('\n', end='')
@@ -147,7 +150,7 @@ INDICES_2 = {3, 4, 5}
 INDICES_3 = {6, 7, 8}
 NUMBERS = set(range(1, 10))
 
-# Extract the grids in the linked file:
+# Extract the grids out of the linked file:
 grids: list[list[list[int]]] = []
 with open('p096_sudoku.txt') as file_object:
     for i, line in enumerate(file_object):
@@ -158,8 +161,6 @@ with open('p096_sudoku.txt') as file_object:
             current_grid.append(current_row)
             if i % 10 == 9:
                 grids.append(current_grid)
-
-N = len(grids)
 
 # grid = grids[7]
 # display_grid(solve(grid))
